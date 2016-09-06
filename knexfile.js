6c3f1036e217c09d1b11322469b4a5b64e125e21
Sashko Stubailo <sashko@stubailo.com>
2016-09-06 15:09:51 -0700

    Style

--- knexfile.js
+++ knexfile.js
@@ -6,9 +6,9 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './dev.sqlite3',
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
   },
   //
   // production: {
