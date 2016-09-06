e5146cb67d9bf983fd105921da0acf4c86d83963
Sashko Stubailo <sashko@stubailo.com>
2016-09-06 14:33:32 -0700

    Add descriptions to SQL schema

--- api/sql/schema.js
+++ api/sql/schema.js
@@ -4,24 +4,48 @@ export const schema = [`
 
 # A comment about an entry, submitted by a user
 type Comment {
+  # The GitHub user who posted the comment
   postedBy: User!
+
+  # A timestamp of when the comment was posted
   createdAt: Float! # Actually a date
+
+  # The text of the comment
   content: String!
+
+  # The repository which this comment is about
   repoName: String!
 }
 
+# XXX to be removed
 type Vote {
   vote_value: Int!
 }
 
+# Information about a GitHub repository submitted to GitHunt
 type Entry {
+  # Information about the repository from GitHub
   repository: Repository!
+
+  # The GitHub user who submitted this entry
   postedBy: User!
+
+  # A timestamp of when the entry was submitted
   createdAt: Float! # Actually a date
+
+  # The score of this repository, upvotes - downvotes
   score: Int!
+
+  # Comments posted about this repository
   comments: [Comment]! # Should this be paginated?
+
+  # The number of comments posted about this repository
   commentCount: Int!
+
+  # The SQL ID of this entry
   id: Int!
+
+  # XXX to be changed
   vote: Vote!
 }
 
