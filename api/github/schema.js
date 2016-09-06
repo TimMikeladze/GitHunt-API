14f8640a88959f02e7495415ed1af82195203f62
Sashko Stubailo <sashko@stubailo.com>
2016-09-06 14:39:09 -0700

    Add descriptions to GitHub schema

--- api/github/schema.js
+++ api/github/schema.js
@@ -1,25 +1,40 @@
 import { property } from 'lodash';
 
 export const schema = [`
-# This uses the exact field names returned by the GitHub API for simplicity
+# A repository object from the GitHub API. This uses the exact field names returned by the
+# GitHub API for simplicity, even though the convention for GraphQL is usually to camel case.
 type Repository {
+  # Just the name of the repository, e.g. GitHunt-API
   name: String!
+
+  # The full name of the repository with the username, e.g. apollostack/GitHunt-API
   full_name: String!
+
+  # The description of the repository
   description: String
+
+  # The link to the repository on GitHub
   html_url: String!
+
+  # The number of people who have starred this repository on GitHub
   stargazers_count: Int!
+
+  # The number of open issues on this repository on GitHub
   open_issues_count: Int
 
-  # We should investigate how best to represent dates
-  created_at: String!
-
+  # The owner of this repository on GitHub, e.g. apollostack
   owner: User
 }
 
-# Uses exact field names from GitHub for simplicity
+# A user object from the GitHub API. This uses the exact field names returned from the GitHub API.
 type User {
+  # The name of the user, e.g. apollostack
   login: String!
+
+  # The URL to a directly embeddable image for this user's avatar
   avatar_url: String!
+
+  # The URL of this user's GitHub page
   html_url: String!
 }
 `];
