4d527bb7705e786b81c3f8840152a384f18858ef
Dhaivat Pandya <dhaivat@meteor.com>
2017-01-05 14:45:55 -0800

    added a config file

---
+++ api/config.js
@@ -0,0 +1,9 @@
+export default {
+  // If set to to true, GitHunt will use `extractgql` in order to
+  // map query ids received from the client to GraphQL documents.
+  //
+  // Note that the same option must be enabled on the client
+  // and the extracted_queries.json file in both the client and API server
+  // must be the same.
+  persistedQueries: true,
+};
