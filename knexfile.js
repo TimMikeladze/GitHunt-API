eb8d1ab6e47b7e96f8ca09ed01bd5dad9c279305
Caleb Meredith <calebmeredith8@gmail.com>
2017-02-03 13:19:47 -0500

    use postgres in production

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,10 @@
 // Since Knex always runs this file first, all of our seeds and migrations are babelified.
 require('babel-register');
 
+const { parse } = require('pg-connection-string');
+
+const { PG_URL } = process.env;
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -8,5 +12,9 @@ module.exports = {
       filename: './dev.sqlite3',
     },
     useNullAsDefault: true,
+  },
+  production: PG_URL && {
+    client: 'pg',
+    connection: Object.assign({}, parse(PG_URL), { ssl: true }),
   },
 };
