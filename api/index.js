6b81988c0d6ef82776e1359afbb302ff6774d524
Caleb Meredith <calebmeredith8@gmail.com>
2017-02-09 09:47:36 -0500

    debug the headers

--- api/index.js
+++ api/index.js
@@ -51,6 +51,8 @@ app.use(
 setUpGitHubLogin(app);
 
 app.use('/graphql', graphqlExpress((req) => {
+  console.log(req.headers)
+
   // Get the query, the same way express-graphql does it
   // https://github.com/graphql/express-graphql/blob/3fa6e68582d6d933d37fa9e841da5d2aa39261cd/src/index.js#L257
   const query = req.query.query || req.body.query;
