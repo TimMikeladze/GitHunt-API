f19b6e8d6f00b41e77ebcf68ad1dd2a85f6d6242
James Baxley <james@meteor.com>
2017-09-19 16:35:35 -0400

    update query for caching and skip broken test

--- api/engineConfig.js
+++ api/engineConfig.js
@@ -1,26 +1,64 @@
 export default {
   stores: [
     {
-      name: 'standardCache',
+      name: "standardCache",
       epoch: 1,
-      timeout: '1s',
+      timeout: "1s",
     },
   ],
   operations: [
     {
       signature: `
-        query CurrentUserForLayout {
-          currentUser {
+        query Feed($type: FeedType!, $offset: Int) {
+          feed(type: $type, offset: $offset, limit: $limit) {
             __typename
-            avatar_url
+            ...FeedEntry
+          }
+        }
+        fragment FeedEntry on Entry {
+          __typename
+          id
+          commentCount
+          repository {
+            __typename
+            full_name
+            html_url
+            owner {
+              __typename
+              avatar_url
+            }
+          }
+          ...VoteButtons
+          ...RepoInfo
+        }
+        fragment VoteButtons on Entry {
+          __typename
+          score
+          vote {
+            __typename
+            vote_value
+          }
+        }
+        fragement RepoInfo on Entry {
+          __typename
+          createdAt
+          repository {
+            __typename
+            description
+            stargazers_count
+            open_issues_count
+          }
+          postedBy {
+            __typename
+            html_url
             login
           }
         }
       `,
       caches: [
         {
-          ttl: 300000,
-          store: 'standardCache',
+          ttl: 60000,
+          store: "standardCache",
         },
       ],
     },
